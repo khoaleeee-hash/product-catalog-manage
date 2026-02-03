7a86362fcc640b0b9ff1a0f9e6dbe4e98a3416ec
Điền Trần <dientran0410@gmail.com>
2026-02-03 22:36:15 +0700

    update code

--- BE/src/main/java/com/example/project/config/SecurityConfig.java
+++ BE/src/main/java/com/example/project/config/SecurityConfig.java
@@ -93,7 +93,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://product-catalog-management-system-f.vercel.app"
+                "https://*.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
